0c9142d2d43502fac72f68b265f32097bb391610
Annie Huang <annie.hn@gmail.com>
2021-01-08 10:54:53 +1100

    Spelling error, children

--- src/components/Optimization/ParentOne.js
+++ src/components/Optimization/ParentOne.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-const ParentOne = ({ chilren }) => {
+const ParentOne = ({ children }) => {
   const [count, setCount] = useState(0);
 
   console.log('ParentOne Render');
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
-      {chilren}
+      {children}
     </div>
   );
 };
