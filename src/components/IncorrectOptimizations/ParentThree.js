6244a079af4a0b65ea2549e2b392040c7b9e0b0a
Annie Huang <annie.hn@gmail.com>
2021-01-08 15:34:16 +1100

    If your {children} is NOT pure text, e.g. <MemoizedChildThree name={name}><strong>Hello</strong></MemoizedChildThree>, change on parent state will cause child to rerender

--- src/components/IncorrectOptimizations/ParentThree.js
+++ src/components/IncorrectOptimizations/ParentThree.js
@@ -7,13 +7,25 @@ const ParentThree = () => {
 
   console.log('ParentThree Render');
 
-  // If the {children} is pure text like: <MemoizedChildThree name={name}>Hello</MemoizedChildThree>
-  // When you click on setCount button (1st button), the child will not render.
+  /* If the {children} is pure text like: <MemoizedChildThree name={name}>Hello</MemoizedChildThree>
+     When you click on setCount button (1st button), the child will NOT render.
+
+     If the {children} is not pure text like:
+      <MemoizedChildThree name={name}>
+        <strong>Hello</strong>
+      </MemoizedChildThree>
+     When you click on setCount button (1st button), the child will render.
+
+     Conclusion: if you have children in your component, no point to wrap your component into React.Memo
+   */
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
       <button onClick={() => setName('Codevolution')}>Change name</button>
-      <MemoizedChildThree name={name}>Hello</MemoizedChildThree>
+      {/*<MemoizedChildThree name={name}>Hello</MemoizedChildThree>*/}
+      <MemoizedChildThree name={name}>
+        <strong>Hello</strong>
+      </MemoizedChildThree>
     </div>
   );
 };
