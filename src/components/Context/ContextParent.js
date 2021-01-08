870bc26dc35bde6932f378be0af7edf4001c8a26
Annie Huang <annie.hn@gmail.com>
2021-01-08 17:52:14 +1100

    Create Context in ContextParent.js and wrapped <ChildA /> inside the Provider

--- src/components/Context/ContextParent.js
+++ src/components/Context/ContextParent.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { ChildA } from './ContextChildren';
 
+export const CountContext = React.createContext();
+const CountProvider = CountContext.Provider;
+
 const ContextParent = () => {
   const [count, setCount] = useState(0);
 
@@ -8,7 +11,9 @@ const ContextParent = () => {
   return (
     <>
       <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
-      <ChildA />
+      <CountProvider value={count}>
+        <ChildA />
+      </CountProvider>
     </>
   );
 };
