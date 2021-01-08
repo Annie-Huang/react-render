f87a6dddb4cf3c5f87588228a76bb53dd3be603c
Annie Huang <annie.hn@gmail.com>
2021-01-08 15:48:40 +1100

    If you have newDate, or Math.random in the child component, chance are, you don't want to wrap the component into Reac.memo.

--- src/components/IncorrectOptimizations/ParentThree.js
+++ src/components/IncorrectOptimizations/ParentThree.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import MemoizedChildThree from './ChildThree';
+// import MemoizedChildThree from './ChildThree';
+import MemoizedChildFour from './ChildFour';
 
 const ParentThree = () => {
   const [count, setCount] = useState(0);
@@ -18,14 +19,21 @@ const ParentThree = () => {
 
      Conclusion: if you have children in your component, no point to wrap your component into React.Memo
    */
+  /*
+     As for <MemoizedChildFour name={name} /> you can tell it's incorrect because
+     the intention is probably when count is clicked, update the time stamp showing as well.
+   */
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
       <button onClick={() => setName('Codevolution')}>Change name</button>
       {/*<MemoizedChildThree name={name}>Hello</MemoizedChildThree>*/}
-      <MemoizedChildThree name={name}>
-        <strong>Hello</strong>
-      </MemoizedChildThree>
+
+      {/*<MemoizedChildThree name={name}>*/}
+      {/*  <strong>Hello</strong>*/}
+      {/*</MemoizedChildThree>*/}
+
+      <MemoizedChildFour name={name} />
     </div>
   );
 };
